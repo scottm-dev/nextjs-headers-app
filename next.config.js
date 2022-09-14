24196e693e7c40de167ea1a329b749a7aaa06971
Scott McDaniel <mcscottm@amazon.com>
2022-09-14 15:03:48 -0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'X-About-Custom-Header',
+            value: 'about_header_value',
+          },
+        ],
+      },
+      {
+        source: '/news/:id',
+        headers: [
+          {
+            key: 'X-News-Custom-Header',
+            value: 'news_header_value',
+          },
+        ],
+      },
+    ]
+  },
+}
