15dd47151dc5b7fef67bbee39ac3166ece01c6f4
Scott McDaniel <mcscottm@amazon.com>
2022-09-14 15:21:05 -0700

    fixing

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/about',
-        headers: [
-          {
-            key: 'X-About-Custom-Header',
-            value: 'about_header_value',
-          },
-        ],
-      },
-      {
-        source: '/news/:id',
-        headers: [
-          {
-            key: 'X-News-Custom-Header',
-            value: 'news_header_value',
-          },
-        ],
-      },
-    ]
-  },
-}
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+    env: {
+        customKey: 'pickles-gm2',
+    },
+  }
+  
+  module.exports = nextConfig
